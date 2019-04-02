63e304306c646ac3b8ac71ecb275eba926ffc064
SmartDengg <Hi4Joker@gmail.com>
2019-04-02 15:03:38 +0800

    add colored log

---
+++ click-debounce-gradle-plugin/src/main/groovy/com/smartdengg/plugin/ColoredLogger.groovy
@@ -0,0 +1,20 @@
+package com.smartdengg.plugin;
+
+class ColoredLogger {
+
+  private static final String ANSI_RESET = "\u001B[0m"
+  private static final String ANSI_GREEN = "\u001B[32m"
+  private static final String ANSI_YELLOW = "\u001B[33m"
+
+  static def logGreen(String text) {
+    println "${ANSI_GREEN}${text}${ANSI_RESET}"
+  }
+
+  static def logYellow(String text) {
+    println "${ANSI_YELLOW}${text}${ANSI_RESET}"
+  }
+
+  static def log(String text) {
+    println text
+  }
+}
